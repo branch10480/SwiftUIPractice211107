c38a6eadef14bfd55d38039e059532f8fb2dd63d
branch10480 <toshiharu.imaeda@gmail.com>
2021-11-07 23:38:22 +0900

    Initial Commit

---
+++ SwiftUIPractice211107/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPractice211107
+//
+//  Created by branch10480 on 2021/11/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIPractice211107/SwiftUIPractice211107App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPractice211107App.swift
+//  SwiftUIPractice211107
+//
+//  Created by branch10480 on 2021/11/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPractice211107App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
